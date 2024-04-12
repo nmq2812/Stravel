dfae3c1f188c76325b34a0726d33c43635285b2e
21020383-NGUYENMINHQUAN <21020383@vnu.edu.vn>
2024-04-12 17:26:03 +0700

    change color

--- app/src/main/java/com/example/stravel/MainActivity.kt
+++ app/src/main/java/com/example/stravel/MainActivity.kt
@@ -14,8 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-       setContent { 
-           Greeting(name = "Stravel")
+       setContent {
+           StravelTheme {
+               Greeting("Android")
+           }
        }
     }
 }
