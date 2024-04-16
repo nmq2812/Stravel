e7d7b41ab608057bed65df6d002bc8127575a070
21020383-NGUYENMINHQUAN <nguyenminhquan341@gmail.com>
2024-04-16 11:48:37 +0700

    completed bottom nav
    nghỉ giỗ tổ thôi

--- app/src/main/java/com/example/stravel/components/AppNavigation.kt
+++ app/src/main/java/com/example/stravel/components/AppNavigation.kt
@@ -1,11 +1,13 @@
 package com.example.stravel.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,37 +45,53 @@ fun AppNavigation() {
     Scaffold (
         modifier = Modifier.padding(16.dp),
         bottomBar = {
-            NavigationBar(modifier = Modifier
-                .fillMaxWidth()
-                .shadow(elevation = 1.dp, shape = RoundedCornerShape(100.dp), spotColor = Color.Transparent)
-            ) {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
+            val mainColor = Color(0xFF176FF2)
+            NavigationBar(
+                containerColor = Color.Transparent,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    //.background(mainColor)
+                    .padding(bottom = 8.dp)
+                    .shadow(elevation = 3.dp, shape = RoundedCornerShape(100.dp), spotColor = mainColor),
+                    content = {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
 
 
-                listOfNavItems.forEach {NavItem ->
-                    val condition = currentDestination?.hierarchy?.any {it.route == NavItem.route} == true
-                    NavigationBarItem(
-                        selected = condition,
-                        onClick = {
-                                  navController.navigate(NavItem.route) {
-                                      popUpTo(navController.graph.findStartDestination().id) {
-                                          saveState = true
-                                      }
-                                      launchSingleTop = true
-                                      restoreState = true
-                                  }
-                        },
-                        icon = {
-                            if (condition) {
-                                Icon(painter = painterResource(id = NavItem.iconSelected), contentDescription = null)
-                            } else {
-                                Icon(painter = painterResource(id = NavItem.iconNotSelected), contentDescription = null)
-                            }
+                        listOfNavItems.forEach {navItem ->
+                            val condition = currentDestination?.hierarchy?.any {it.route == navItem.route} == true
+                            NavigationBarItem(
+                                modifier = Modifier.background(Color.Transparent),
+                                selected = condition,
+                                colors = NavigationBarItemColors(
+                                    selectedIconColor = mainColor,
+                                    selectedTextColor = Color.Transparent,
+                                    disabledIconColor = mainColor,
+                                    disabledTextColor = Color.Transparent,
+                                    unselectedIconColor = Color.Gray,
+                                    unselectedTextColor = Color.Transparent,
+                                    selectedIndicatorColor = Color.Transparent,
+                                ),
+                                onClick = {
+                                    navController.navigate(navItem.route) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                },
+                                icon = {
+                                    if (condition) {
+                                        Icon(painter = painterResource(id = navItem.iconSelected), contentDescription = null, tint = mainColor)
+                                    } else {
+                                        Icon(painter = painterResource(id = navItem.iconNotSelected), contentDescription = null)
+                                    }
+                                }
+                            )
                         }
-                    )
-                }
-            }
+                    }
+            )
         }
     ) { paddingValues ->
         NavHost(
